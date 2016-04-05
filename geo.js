fa6ec6cc6f80d2c4f77fd4e1744f610e098c9f6b
Clorr <colin@imac-de-colin.home>
2016-04-05 22:00:27 +0200

    protection si data est null

--- geo.js
+++ geo.js
@@ -8,7 +8,7 @@ module.exports.geocode = function(address, callback, error_callback) {
 		callback(cache[address]);
 	} else {
 		geocoder.geocode(address, function ( err, data ) {
-			if(data.results && data.results[0] && data.results[0].geometry && data.results[0].geometry.location) {
+			if(data && data.results && data.results[0] && data.results[0].geometry && data.results[0].geometry.location) {
 				var res = data.results[0].geometry.location;
 				cache[address] = res
 				callback(res);
